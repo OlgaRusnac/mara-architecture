c529a2c8728eb16b3507c82808022e045e09ca07
Olga Rusnac <olga.v.rusnac@gmail.com>
2023-05-09 07:55:43 +0200

    first day

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,20 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def projet
+  end
+
+  def service
+  end
+
+  def about
+  end
+
+  # def process
+  # end
+
+  def contact
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  get "project", to: "pages#project"
+  get "service", to: "pages#service"
+  get "about", to: "pages#about"
+  get "project", to: "pages#project"
+  get "proces", to: "pages#proces"
+  get "contact", to: "pages#contact"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
